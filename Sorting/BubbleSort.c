81210eaec8878e6f6271fa69b9efc85227be2bea
Harshvardhan Singh <hvsingh46@gmail.com>
2025-12-26 02:21:43 +0530

    Update BubbleSort.c

--- Sorting/BubbleSort.c
+++ Sorting/BubbleSort.c
@@ -3,7 +3,7 @@ int main(){
     int arr[7]={4,3,5,6,7,8,9}; 
     int n=7;
 for(int i=0;i<n-1;i++){
-for(int j=0;j<n-1-i;j++){
+for(int j=0;j<=n-1-i;j++){
  if(arr[j]>arr[j+1]){int  temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
